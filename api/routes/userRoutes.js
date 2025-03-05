ad65546cd97fff1f312b5107414c7bcbae500760
Gwara106 <lucky.prajapati715@gmail.com>
2025-03-05 18:50:27 +0545

    updating routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,7 +3,6 @@ import { protectRoute } from "../middleware/auth.js";
 import { updateProfile } from "../controllers/userController.js";
 
 const router = express.Router();
-
 router.put("/update", protectRoute, updateProfile);
 
 export default router;
